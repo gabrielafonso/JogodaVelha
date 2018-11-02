7bdbc56381512f57e7672954b103cf8273d2604d
“Janaine <janainesantosifal@gmail.com>
2018-11-02 18:27:40 -0300

    Criação e implementação da Classe abstrata Jogador

---
+++ src/Jogador.java
@@ -0,0 +1,22 @@
+
+public abstract class Jogador {
+	 protected int[] tentativa = new int[2];
+	    protected int jogador;
+
+	    
+	    public Jogador(int jogador){
+	        this.jogador = jogador;
+	    }
+	    
+	    public abstract void jogar(Tabuleiro tabuleiro);
+	    
+	    public abstract void Tentativa(Tabuleiro tabuleiro);
+
+	    public boolean checarTentativa(int[] tentativa, Tabuleiro tabuleiro){
+	        if(tabuleiro.getPosicao(tentativa) == 0)
+	            return true;
+	        else
+	            return false;
+	            
+	    }
+}
